86c70fe7da1fab3320786b0502168e5ff604b41e
jhamlin-ufl <jhamlin@ufl.edu>
2024-04-30 11:41:11 -0400

    updated main.py, currently broken

--- dacdaq/main.py
+++ dacdaq/main.py
@@ -1,7 +1,10 @@
 import sys
-from PyQt6 import QtWidgets, uic
+from PyQt6.QtWidgets import QApplication, QWidget, uic
 
-app = QtWidgets.QApplication(sys.argv)
-window = uic.loadUi("ui/dacdaq.ui")
-window.show()
-app.exec()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = uic.loadUi("ui/dacdaq.ui")
+    window.show()
+
+    app.exec()
