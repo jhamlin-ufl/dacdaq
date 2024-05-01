e71d7b75f1defa39029d46aa6423a94056f98683
jhamlin-ufl <jhamlin@ufl.edu>
2024-05-01 00:17:41 -0400

    MWE pyqtgraph

--- dacdaq/main.py
+++ dacdaq/main.py
@@ -1,11 +1,28 @@
 import sys
+import pyqtgraph as pg
+import numpy as np
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QWidget
 
 if __name__ == "__main__":
+    x = np.random.random(10)
+    y = np.random.random(10)
+
     app = QApplication(sys.argv)
 
     window = uic.loadUi("ui/dacdaq.ui")
+
+    plotWidget = pg.PlotWidget()
+
+    # Set the title of the plot
+    plotWidget.setWindowTitle("PyQtGraph Example")
+
+    # Add a curve to the plot
+    plotWidget.plot(x, y)
+
+    # Show the plot
+    plotWidget.show()
+
     window.show()
 
     app.exec()
